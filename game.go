7efa9c9c99521ff978c8bb4c1e5e294458cf8499
OisinA <oisinaylward@gmail.com>
2017-11-06 21:24:53 +0000

    Fixed weird indentation.

--- game.go
+++ game.go
@@ -12,61 +12,61 @@ type CharClass struct {
 }
 
 type Player struct {
-  name string
-  class CharClass
-  health int
+    name string
+    class CharClass
+    health int
 }
 
 type Enemy struct {
-  name string
-  difficulty int
-  health int
+    name string
+    difficulty int
+    health int
 }
 
 func ClearLine() {
-  for i := 0; i < 34; i++ {
-    fmt.Print("-")
-  }
-  fmt.Print("\n")
+    for i := 0; i < 34; i++ {
+      fmt.Print("-")
+    }
+    fmt.Print("\n")
 }
 
 func DisplayName(p Player) {
-  fmt.Print("You are playing as ", p.name, " the ", p.class.name, ".\n")
+    fmt.Print("You are playing as ", p.name, " the ", p.class.name, ".\n")
 }
 
 var terminate = false
 
 func main() {
 
-  reader := bufio.NewReader(os.Stdin)
-  ClearLine()
-  fmt.Print("Please enter your name: ")
+    reader := bufio.NewReader(os.Stdin)
+    ClearLine()
+    fmt.Print("Please enter your name: ")
 
-  text, _ := reader.ReadString('\n')
-  text = text[:len(text) - 2]
-  ClearLine()
-  p := Player{text, CharClass{"Wizard", 0.25}, 20}
+    text, _ := reader.ReadString('\n')
+    text = text[:len(text) - 2]
+    ClearLine()
+    p := Player{text, CharClass{"Wizard", 0.25}, 20}
 
-  DisplayName(p)
+    DisplayName(p)
 
-  enemy := Enemy{"Zombie", 1, 20}
+    enemy := Enemy{"Zombie", 1, 20}
 
-  ClearLine()
+    ClearLine()
 
-  fmt.Print("You stumble across a ", enemy.name, ".\n")
+    fmt.Print("You stumble across a ", enemy.name, ".\n")
 
-  for {
-    if terminate {
-      break
+    for {
+      if terminate {
+        break
+      }
+
+      fmt.Print("What do you do?\n> ")
+
+      action, _ := reader.ReadString('\n')
+      fmt.Print(action)
+
     }
 
-    fmt.Print("What do you do?\n> ")
-
-    action, _ := reader.ReadString('\n')
-    fmt.Print(action)
-
-  }
-
-  ClearLine()
+    ClearLine()
 
 }

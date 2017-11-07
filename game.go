344220d0f7dc76d0f27eef81a9889bc8580ad346
OisinA <oisinaylward@gmail.com>
2017-11-07 19:33:18 +0000

    Adding basic action parsing, allows the player to attack the enemy.

--- game.go
+++ game.go
@@ -4,6 +4,7 @@ import (
     "bufio"
     "fmt"
     "os"
+    "strings"
 )
 
 type CharClass struct {
@@ -23,6 +24,28 @@ type Enemy struct {
     health int
 }
 
+var player Player
+var enemy Enemy
+
+func (p *Player) Damage(damage int) {
+    p.health = p.health - damage
+}
+
+func (e *Enemy) Damage(damage int) {
+    e.health = e.health - damage
+}
+
+func (p Player) Attack(enemy *Enemy, damage int) {
+    enemy.Damage(damage)
+    fmt.Print("You damaged the enemy for ", damage, " damage.\n")
+}
+
+func (p *Player) ParseAction(action string) {
+    if action == "attack" {
+        p.Attack(&enemy, 3)
+    }
+}
+
 func ClearLine() {
     for i := 0; i < 34; i++ {
       fmt.Print("-")
@@ -30,8 +53,29 @@ func ClearLine() {
     fmt.Print("\n")
 }
 
-func DisplayName(p Player) {
+func (p Player) DisplayName() {
     fmt.Print("You are playing as ", p.name, " the ", p.class.name, ".\n")
+}
+
+func CheckHealth() bool {
+    return player.health <= 0 || enemy.health <= 0
+}
+
+func EndGame() {
+    phealth := player.health
+    ehealth := enemy.health
+
+    fmt.Print("The game is over.\n")
+
+    if(phealth <= 0) {
+        fmt.Print("You have died! Unfortunate.\n")
+        return
+    }
+
+    if(ehealth <= 0) {
+        fmt.Print("You have slain the ", enemy.name, "!\n")
+        return
+    }
 }
 
 var terminate = false
@@ -45,26 +89,45 @@ func main() {
     text, _ := reader.ReadString('\n')
     text = text[:len(text) - 2]
     ClearLine()
-    p := Player{text, CharClass{"Wizard", 0.25}, 20}
+    player = Player{text, CharClass{"Wizard", 0.25}, 20}
 
-    DisplayName(p)
+    player.DisplayName()
 
-    enemy := Enemy{"Zombie", 1, 20}
+    enemy = Enemy{"Zombie", 1, 20}
 
     ClearLine()
 
     fmt.Print("You stumble across a ", enemy.name, ".\n")
 
     for {
-      if terminate {
-        break
-      }
+        if terminate {
+            EndGame()
+            break
+        }
 
-      fmt.Print("What do you do?\n> ")
+        fmt.Print("What do you do?\n> ")
 
-      action, _ := reader.ReadString('\n')
-      fmt.Print(action)
+        action, _ := reader.ReadString('\n')
+        action = action[:len(action) - 2]
+        action = strings.ToLower(action)
+        ClearLine()
 
+        validActions := map[string]bool {
+            "attack": true,
+            "flee": true,
+        }
+
+        if validActions[action] {
+              player.ParseAction(action)
+        } else {
+              continue
+        }
+
+        fmt.Print("You have ", player.health, " health remaining.\n")
+        fmt.Print("The enemy has ", enemy.health, " health remaining.\n")
+        ClearLine()
+
+        terminate = CheckHealth()
     }
 
     ClearLine()
